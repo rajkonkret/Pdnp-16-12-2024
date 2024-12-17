bf63accfd64c9ae565f15a2620e3ddb6580dc652
rajkonkret <rajkonkret660@gmail.com>
2024-12-17 11:57:01 +0100

    [2 4] update typy_danych_2_lista.py

--- day_2/typy_danych_2_lista.py
+++ day_2/typy_danych_2_lista.py
@@ -97,3 +97,71 @@ print(lista)
 print(lista.pop(5))  # Bogdan
 print(lista.pop(-3))  # Marek
 print(lista.pop())  # Asia, usunie ostatni element z listy
+
+a = 1
+b = 3
+a = b
+print(f"{a=}, {b=}")  # a=3, b=3
+b = 7
+print(f"{a=}, {b=}")  # a=3, b=7
+
+lista_2 = lista  # kopia referencji,  adresu w pamieci
+lista_copy = lista.copy()  # kopia elementów listy
+print(lista)  # ['Radek', 'Krzysztof', 'Zenek', 'Tomek', 'Żaklina']
+print(lista_2)  # ['Radek', 'Krzysztof', 'Zenek', 'Tomek', 'Żaklina']
+lista.clear()  # usunięcie elementów z listy
+print(lista)  # []
+print(lista_2)  # []
+print(lista_copy)  # ['Radek', 'Krzysztof', 'Zenek', 'Tomek', 'Żaklina']
+print(id(lista))
+print(id(lista_2))
+print(id(lista_copy))
+# 1759792075136
+# 1759792075136
+# 1759795494656
+
+liczby = [54, 999, 34, 22, 12.34, 687]
+print(liczby)  # [54, 999, 34, 22, 12.34, 687]
+print(type(liczby))  # <class 'list'>
+
+liczby.sort()
+print(liczby)  # [12.34, 22, 34, 54, 687, 999]
+
+liczby = [54, 999, 34, 22, 12.34, 687, 'A']
+print(liczby)  # [54, 999, 34, 22, 12.34, 687, 'A']
+# liczby.sort()  # TypeError: '<' not supported between instances of 'str' and 'int'
+
+print(ord("A"))  # kod ascii litery "A" 65
+
+lista_osob = ['radek', 'ola', 'maciek', 'olek', 'marta', 'aravind']
+lista_osob.sort()
+print(lista_osob)  # ['aravind', 'maciek', 'marta', 'ola', 'olek', 'radek']
+
+lista_osob.sort(reverse=True)
+print(lista_osob)  # ['radek', 'olek', 'ola', 'marta', 'maciek', 'aravind']
+
+lista_osob.reverse()
+print(lista_osob)  # ['aravind', 'maciek', 'marta', 'ola', 'olek', 'radek']
+
+# tylko wyswietlenie w odwrotnej kolejności
+print(lista_osob[::-1])  # ['radek', 'olek', 'ola', 'marta', 'maciek', 'aravind']
+
+liczby[3] = 666
+print(liczby[0:3])
+print(liczby[-2])
+print(liczby)  # [54, 999, 34, 666, 12.34, 687, 'A']
+
+del liczby  # usunięcie listy z pamięci
+# print(liczby) # NameError: name 'liczby' is not defined
+
+# rozpakowanie sekwencji
+tekst = 'Pyth on.'
+lista1 = list(tekst)
+print(lista1)  # ['P', 'y', 't', 'h', ' ', 'o', 'n', '.']
+
+lista2 = [tekst]
+print(lista2)  # ['Pyth on.']
+
+krotka = tuple(lista_copy)  # tuple() - rzutowanie (zamiana) na krotkę, tuple
+print(type(krotka))  # <class 'tuple'>
+print(krotka)  # ('Radek', 'Krzysztof', 'Zenek', 'Tomek', 'Żaklina')
