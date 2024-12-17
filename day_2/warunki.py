b365c6a5b06b28d350ab28ea8ba533d40027f7a3
rajkonkret <rajkonkret660@gmail.com>
2024-12-17 15:53:48 +0100

    [2 10] add warunki.py

---
+++ day_2/warunki.py
@@ -0,0 +1,21 @@
+# instrukcje warunkowe
+# instrukcje sterowania przepływem programu
+# if
+# w zależności od warunku wykona jeden lub drugi blok programu
+# warunek musi zwracac bool
+
+odp = True
+print(bool(odp))  # True
+# odp = False
+if odp:
+    # blok kodu wgdy warunek True
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+
+print("Dalsza część programu")
