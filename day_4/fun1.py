07724fcee965e0ae9b0f2dd4cd2f3cbd7f65ab15
rajkonkret <rajkonkret660@gmail.com>
2024-12-19 12:32:35 +0100

    [4 6] update fun1

--- day_4/fun1.py
+++ day_4/fun1.py
@@ -17,6 +17,27 @@ def dodaj2(a, b):  # dwa obowiązkowe argumenty
     print(a + b)  # zmienne lokalne
 
 
+# symulujemy przeciązanie funkcji różną liczbą argumentów
+def odejmij(a, b, c=0):  # argument c ma wartosc domyslna
+    print(a - b - c)
+
+
 dodaj()  # 14
 # dodaj2() # TypeError: dodaj2() missing 2 required positional arguments: 'a' and 'b'
-dodaj2(5, 7) # 12
+
+# argumenty po pozycji
+dodaj2(5, 7)  # 12
+odejmij(1, 2)
+odejmij(1, 2, 3)
+
+# argumenty po nazwie
+odejmij(b=9, a=8)
+odejmij(b=9, a=8, c=90)
+
+# argumenty mieszane
+odejmij(1, c=10, b=67)
+# odejmij(a=1, c=10, 67) # SyntaxError: positional argument follows keyword argument
+
+print(dodaj())
+print(dodaj() + dodaj2(5, 9))
+# TypeError: unsupported operand type(s) for +: 'NoneType' and 'NoneType'
