fa1272aaeb686c383e48ba342c2672692d2aed4a
rajkonkret <rajkonkret660@gmail.com>
2024-12-20 15:31:34 +0100

    [5 15] add okno

---
+++ day_5/okno.py
@@ -0,0 +1,21 @@
+import tkinter
+
+
+class MyGui:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+
+        self.label = tkinter.Label(self.main_window, text='Witaj Świecie')
+        self.label2 = tkinter.Label(self.main_window, text='Witaj Świecie')
+        self.label3 = tkinter.Label(self.main_window, text='Witaj Świecie')
+        self.label4 = tkinter.Label(self.main_window, text='Witaj Świecie')
+
+        self.label.pack(side="left")
+        self.label2.pack(side="right")
+        self.label3.pack(side="top")
+        self.label4.pack(side="bottom")
+
+        tkinter.mainloop()
+
+
+my_gui = MyGui()
